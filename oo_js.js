89ddd160d084885499caf41f940f50b0b7eb9fe5
CJosueAK47 <83555240+CJosueAK47@users.noreply.github.com>
2023-10-02 10:16:17 -0300

    Update oo_js.js
    agora vai

--- oo_js.js
+++ oo_js.js
@@ -1,3 +1,4 @@
+//Programadores 
 function Funcionario(nome, idade, setor, cargo, remuneracao) {
     this.nome = nome;
     this.idade = idade;
@@ -6,15 +7,34 @@ function Funcionario(nome, idade, setor, cargo, remuneracao) {
     this.remuneracao = remuneracao;
 }
 
-function Estagiario(nome, idade, setor, cargo, remuneracao,tempo_estagio, avaliacao){
-    this.tempo_estagio = tempo_estagio;
-    this.avaliacao = avaliacao;
+function Estagiario(nome, idade, setor, cargo, remuneracao, tempoestagio, avaliacao){
+    Funcionario.call(this, nome, idade, setor, cargo, remuneracao);
 
-    Funcionario.call(this, nome, idade, setor, cargo, remuneracao)
+    this.tempoestagio = tempoestagio;
+    this.avaliacao = avaliacao;
 }
 
-const joao = new Funcionario("Joao Castro Alves", 52, "Aciaria","Supervisor", 5320);
-//const andressa = new Estagiario("Adressa Aguiar", 20, "RH", 1300, 3, 9.0);
+function Candidato_estagiario(nome, idade, setor, cargo, remuneracao, fase){
+    Funcionario.call(this, nome, idade, setor, cargo, remuneracao);
 
+    this.fase = fase;
+
+}
+
+function Freelancer(nome, idade, setor, cargo, remuneracao, meses_servico){
+    Funcionario.call(this, nome, idade, setor, cargo, remuneracao);
+
+    this.meses_servico = meses_servico;
+}
+
+const joao = new Funcionario("Joao Castro Alves", 52, "Back-end","Supervisor", 15320);
+const andressa = new Estagiario("Andressa Aguiar", 20, "RH", "Supervisor", 2300, 3, 9);
+const thamires = new Candidato_estagiario("Thamires Aquino", 24, "Front-end", "candidata", 0, 3);
+const luana = new Freelancer("Luana Maia", 32, "Data Base", "Freelancer", 9000, 5);
+
+
+console.log("Programadores: ");
 console.log(joao);
-//console.log(andressa);
+console.log(andressa);
+console.log(thamires);
+console.log(luana);
